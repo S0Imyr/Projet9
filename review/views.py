a9e4f0d4cb2580241d43329c9b7a9e2c79d33359
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-01 22:17:42 +0200

    reorganization of templates

--- review/views.py
+++ review/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from operator import attrgetter
 from itertools import chain
+from django.views import generic
 
 from review.models import Ticket, Review
+
 
 def flux(request):
     tickets = Ticket.objects.all()
     reviews = Review.objects.all()
     articles = sorted(chain(tickets, reviews),
-                      key=attrgetter('time_created'))
+                      key=attrgetter('time_created'),
+                      reverse=True)
 
-    return render(request, 'flux.html', {'articles': articles})
+    return render(request, 'review/flux.html', {'articles': articles})
