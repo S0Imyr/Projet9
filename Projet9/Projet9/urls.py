6177b5bbdd67787f5410a5a7d739b1cd514ae125
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-04 22:36:22 +0200

    links and forms between authentification page

--- Projet9/Projet9/urls.py
+++ Projet9/Projet9/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('review/', include('review.urls')),
+    path('review/', include('review.urls'), name='review'),
     path('', include('authentification.urls')),
 ]
--- Projet9/authentification/views.py
+++ Projet9/authentification/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
 
 from .forms import CreateUserForm
 
 def login(request):
-    context = {}
+    form = CreateUserForm()
+    context = {'form': form}
     return render(request, 'home.html', context)
 
 
@@ -15,6 +17,9 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
+        else:
+            pass
 
     context = {'form': form}
     return render(request, 'createaccount.html', context)
