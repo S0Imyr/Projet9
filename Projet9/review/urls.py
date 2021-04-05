da705d77c4856ef91db4c9082db53ce7bd90971c
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-05 23:16:41 +0200

    links to add review

--- Projet9/review/urls.py
+++ Projet9/review/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('addticket/', views.create_ticket, name="addticket"),
     path('addticket/<int:id_ticket>', views.create_ticket, name="addticket"),
     path('addreview/', views.create_review, name="addreview"),
-    path('addreview/<int:id_review>', views.create_review, name="addreview"),
+    path('addreview/<int:id_ticket>', views.create_review, name="addreview"),
     path('addticketreview/', views.create_ticketreview, name="addticketreview"),
 ]
