42b3f0b48f98af41b30f1fa5ddaea624db2f6249
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-10 22:34:26 +0200

    radio button for rating

--- Projet9/review/forms.py
+++ Projet9/review/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms.widgets import RadioSelect
 from review.models import Ticket, Review
 
+CHOICES = (("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5))
 
 class TicketForm(forms.ModelForm):
     class Meta:
@@ -13,13 +14,13 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['ticket', 'rating', 'headline', 'body', 'user']
+        widgets = {'rating': RadioSelect(choices=CHOICES)}
 
 
 class TicketReviewForm(forms.Form):
-    choices = (("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5))
     ticket_title = forms.CharField(max_length=128)
     ticket_description = forms.CharField(widget=forms.Textarea)
     ticket_image = forms.ImageField()
-    review_rating = forms.ChoiceField(choices=choices, widget=RadioSelect())
+    review_rating = forms.ChoiceField(choices=CHOICES, widget=RadioSelect())
     review_headline = forms.CharField(max_length=128)
     review_body = forms.CharField(max_length=8192)
